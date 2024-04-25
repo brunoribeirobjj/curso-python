6ab121769515a693d7fc88ceeab485bdd766618c
Bruno Ribeiro <brunoribeirobjj@gmail.com>
2024-04-24 21:00:15 -0300

    Aulas de variáveis, operadores e tipos de dados

---
+++ aula8_exercicio.py
@@ -0,0 +1,13 @@
+nome = 'Bruno'
+sobrenome = 'Santos Ribeiro'
+idade = 34
+anoNascimento = 2024 - idade
+maiorDeIdade = idade >= 18
+altMetros = 1.70
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', anoNascimento)
+print('Maior de idade?', maiorDeIdade)
+print('Altura em metros:', altMetros)
---
+++ aula9_operadoresAritmeticos.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2 # Sempre o retorno é float
+print('Divisão', divisao)
+
+divisaoInteira = 10 // 2.2 # Retorna inteiro e float caso seja dividido por float, arendodando pra baixo, cortando casas decimais.
+print('Divisão inteira', divisaoInteira)
+
+exponenciacao = 2 ** 10 # Elevar um número a outro, potênciação.
+print('Exponenciação', exponenciacao)
+
+modulo = 25 % 5 # Retorna o resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
