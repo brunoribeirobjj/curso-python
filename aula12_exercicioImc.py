515eefc500d27ffb47e500861b7d4fd28a80be67
Bruno Ribeiro <brunoribeirobjj@gmail.com>
2024-04-25 23:21:38 -0300

    Exercício, calculando IMC

---
+++ aula12_exercicioImc.py
@@ -0,0 +1,11 @@
+nome = 'Bruno Ribeiro'
+altura = 1.70
+peso = 85
+imc = peso / altura ** 2
+print(nome, 'tem', altura, 'de altura ',)
+print('pesa', peso, 'quilos e seu imc é' ,)
+print(imc)
+
+# Bruno Ribeiro tem 1.80 de altura,
+# pesa 85 quilos e seu IMC é
+# 29.320987654320987
